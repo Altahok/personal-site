790eee76d2252302be172d4d4838cce0018cf395
Altahok <altahok4@yandex.ru>
2022-02-10 22:12:28 +0400

    10.02.2022

--- modal.js
+++ modal.js
@@ -1,4 +1,4 @@
-// Модальное Окно
+// Модальное Окно "ПОМОЩЬ"
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("helpLink");
 const closeBtn = document.getElementById("closeBtn");
@@ -18,4 +18,48 @@ window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
+}
+
+
+
+// Модальное окно "ВОЙТИ"
+const modalEnt = document.getElementById("enter-wrapper");
+const btnEnter = document.getElementById("enterLink");
+const  closeEnter = document.getElementById("closeEnter");
+
+// Открытие окна
+btnEnter.onclick = function () {
+    modalEnt.style.display = "block";
+}
+
+// Закрытие крестиком
+closeEnter.onclick = function () {
+    modalEnt.style.display = "none";
+}
+
+// // Закрытие на пустое место
+// window.onclick = function (event) {
+//     if (event.target === modal) {
+//         modalEnt.style.display = "none";
+//     }
+// }
+
+
+
+
+// Закрытие через кнопку отправить "Помощь"
+const closebtns = document.getElementById("btnHelp");
+
+closebtns.onclick = function (closed) {
+    modal.style.display = "none";
+    alert("Ваша заявка будет рассмотрена в течение 1 часа.")
+}
+
+
+// Закрытие через кнопку отправить "Авторизация"
+const endButton = document.getElementById("endCl");
+
+endButton.onclick = function () {
+    modalEnt.style.display = "none";
+    alert("Вы успешно авторизировались!")   ;
 }
